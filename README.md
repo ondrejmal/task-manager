2df34dc9d8e3b5d7ec23dc64322aedb131ab1fce
ondrejmal <ondrej.malinak@proton.me>
2024-12-22 08:46:47 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Simple Task Manager Project for CodersLab Workshop.
 
-21 DEC 2024 - 22 DEC 2024
-
 Program is able to:
 - add,
 - remove,
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(ConsoleColors.BLUE + "Task Manager Coderslab" + ConsoleColors.RESET);
